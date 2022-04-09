28aaa5ee775504648fdb199018f62de8777cf40c
Benshalom <benshalom.kariuki@kopokopo.com>
2022-04-09 12:22:01 +0300

    Implement custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+)
+end
 
 
 10.times do |blog|
 Blog.create!(
 title: "My blog post  #{blog}",
-body: "lorem ipsum" 
+body: "lorem ipsum",
+topic_id: Topic.last.id 
 
 )
 end 
@@ -23,15 +22,38 @@ puts "10 blog posts   created"
     )
 end 
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
       title: "Portfolio title: #{portfolio_item}",
-      subtitle:"My great price" ,
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque semper massa neque, quis consectetur ex mollis vitae. Ut ac malesuada velit. Quisque feugiat, mi vitae bibendum hendrerit, neque leo mattis diam, placerat luctus dolor nisl id est. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam sit amet nunc tempor, malesuada dolor fermentum, luctus erat. Nullam in iaculis arcu. Curabitur non arcu arcu. Etiam non nibh consequat, laoreet nisl id, mattis augue. Nulla pulvinar justo non mattis fringilla." ,
+      subtitle:"Ruby on Rails" ,
+      body: "Lorem ipsum dolor sit amet, 
+      consectetur adipiscing elit. Quisque semper massa neque, quis consectetur ex mollis vitae. 
+      Ut ac malesuada velit. Quisque feugiat, mi vitae bibendum hendrerit, 
+      neque leo mattis diam, placerat luctus dolor nisl id est. 
+      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+      Etiam sit amet nunc tempor, malesuada dolor fermentum, luctus erat. Nullam in iaculis arcu. 
+      Curabitur non arcu arcu. Etiam non nibh consequat, laoreet nisl id, mattis augue. 
+      Nulla pulvinar justo non mattis fringilla." ,
       main_image: "https://via.placeholder.com/600.png/09f/fff",
       thumb_image: "https://via.placeholder.com/300.png/09f/fff"
   )
 end 
+1.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item}",
+        subtitle:"Angular" ,
+        body: "Lorem ipsum dolor sit amet, 
+        consectetur adipiscing elit. Quisque semper massa neque, quis consectetur ex mollis vitae. 
+        Ut ac malesuada velit. Quisque feugiat, mi vitae bibendum hendrerit, 
+        neque leo mattis diam, placerat luctus dolor nisl id est. 
+        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+        Etiam sit amet nunc tempor, malesuada dolor fermentum, luctus erat. Nullam in iaculis arcu. 
+        Curabitur non arcu arcu. Etiam non nibh consequat, laoreet nisl id, mattis augue. 
+        Nulla pulvinar justo non mattis fringilla." ,
+        main_image: "https://via.placeholder.com/600.png/09f/fff",
+        thumb_image: "https://via.placeholder.com/300.png/09f/fff"
+    )
+  end 
 
 puts "5 skills created"
 puts "9 portfolio items"
