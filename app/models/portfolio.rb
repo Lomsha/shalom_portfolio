ead43d258d2a94e7d6d181eb16b02da4a1c20bba
Benshalom <benshalom.kariuki@kopokopo.com>
2022-04-09 13:03:54 +0300

    How to set default values in rails: implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,12 @@ class Portfolio < ApplicationRecord
         where(subtitle: 'Angular')
     end
     scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') } 
+    
+    after_initialize :set_defaults
+
+    def set_defaults
+      self.main_image  ||= "https://via.placeholder.com/600.png/09f/fff"
+      self.thumb_image ||=  "https://via.placeholder.com/600.png/09f/fff"
+    end 
+
 end
