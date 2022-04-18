47c09d3b81d4f6d9a3be2ad7c5e0b2b66d35ad5d
Benshalom <benshalom.kariuki@kopokopo.com>
2022-04-18 11:16:26 +0300

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,8 @@ module ApplicationHelper
          content_tag(:p, greeting, class: "source-greeting" )
            end 
     end
+
+     def copyright_generator
+       KarisViewTool::Renderer.copyright 'Benshalom Kariuki','All rights reserved'
+     end 
 end
