2d82e26893a5cd9f638b4a562273188780b4d7bf
Benshalom <benshalom.kariuki@kopokopo.com>
2022-04-19 08:46:29 +0300

    Removed vendor/bundle directory

--- vendor/bundle/ruby/2.5.0/gems/karis_view_tool-0.1.0/lib/karis_view_tool.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "karis_view_tool/version"
-require_relative "karis_view_tool/renderer"
-
-module KarisViewTool
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- vendor/bundle/ruby/2.5.0/gems/karis_view_tool-0.1.0/lib/karis_view_tool/renderer.rb
+++
@@ -1,9 +0,0 @@
-module KarisViewTool
-    class Error < StandardError; end
-
-    class Renderer
-        def self.copyright name, msg 
-             "&copy: #{Time.now.year} | <b>#{name}<b> #{msg}".html_safe
-        end
-   end
-  end
