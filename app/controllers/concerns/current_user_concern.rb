4c26339b19328e14e2c89aa5a0a62c76c5f561e1
Benshalom <benshalom.kariuki@kopokopo.com>
2022-04-19 11:59:47 +0300

    Implemented authorization engine for portfolio items and blogs

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -4,10 +4,16 @@ module  CurrentUserConcern
         super || guest_user
      end 
      def guest_user
-        guest = OpenStruct.new(name: "Guest User", 
-                               first_name: "Guest", 
-                               last_name: "User", 
-                               email: "guests@example.com"
-                               )
+     guest = GuestUser.new
+     guest.name = "GuestUser.new"
+     guest.first_name = "Guest"
+     guest.last_name = "User"
+     guest.email = "guest@example.com"
+     guest
+        # OpenStruct.new(name: "Guest User", 
+        #                        first_name: "Guest", 
+        #                        last_name: "User", 
+        #                        email: "guests@example.com"
+        #                        )
      end 
 end 
