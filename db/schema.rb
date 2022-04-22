0af68b7680917506a4fd88cc5dab9d1b03cde19c
Benshalom <benshalom.kariuki@kopokopo.com>
2022-04-22 05:59:20 +0300

    added installation requirements for petergate

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 2022_04_12_160046) do
-ActiveRecord::Schema.define(version: 2022_03_28_153405) do
-ActiveRecord::Schema.define(version: 2022_03_28_153405) do
-ActiveRecord::Schema.define(version: 2022_04_12_160046) do
-ActiveRecord::Schema.define(version: 2022_03_28_153405) do
-ActiveRecord::Schema.define(version: 2022_04_06_051549) do
+ActiveRecord::Schema.define(version: 2022_04_22_014131) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
     t.string "slug"
     t.integer "status", default: 0
     t.integer "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
     t.index ["topic_id"], name: "index_blogs_on_topic_id"
-    t.string "slug"
-    t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -91,6 +81,7 @@ ActiveRecord::Schema.define(version: 2022_04_06_051549) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
